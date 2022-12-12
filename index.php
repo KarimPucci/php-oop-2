5fc95c172ae1fcaf0c0cfc6614560a26ec8c87f2
Karim <kizpucci@gmail.com>
2022-12-12 16:53:10 +0100

    category

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+include_once __DIR__ . '/models/Category.php';
+include_once __DIR__ . '/models/Product.php';
+include_once __DIR__ . '/models/Food.php';
+include_once __DIR__ . '/models/Toy.php';
+include_once __DIR__ . '/models/Accessory.php';
---
+++ models/Category.php
@@ -0,0 +1,13 @@
+<?php
+class Category {
+    public $name;
+
+    /*
+    @param String $_name
+    */
+
+    public function __construct($_name)
+    {
+        $this->name = $_name;
+    }
+}
